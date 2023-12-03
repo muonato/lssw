14a0d87e034a7f621f1b71d5cb3bb224aa3203dc
Tommi Muona <43796235+muonato@users.noreply.github.com>
2023-12-03 21:29:46 +0200

    Create lssw.sh
    Initial commit

---
+++ lssw.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# muonato/lssw @ GitHub (DEC-2023)
+#
+# Gets installed packages using rpm query
+# + prints out software name and version
+#
+# Usage:
+#       bash lssw.sh <hosts> [package]
+#
+# Parameters:
+#       1: Path to filename or hostname
+#       2: Package name to query (optional)
+#
+# Examples:
+#       bash lssw.sh servers.txt
+#       = query servers listed in file
+#
+#       bash lssw.sh server.domain.net
+#       = query the specified hostname
+#
+#       bash lssw.sh servers.txt php
+#       = query listed servers for PHP
+#
+function get_sw() {
+    echo -e "\n$HOST\n=============="
+    grep -A 0 -e "Name[[:blank:]].*:" -e "Epoch[[:blank:]].*:" -e "Version[[:blank:]].*:" --group-separator=""
+}
+
+# Query all or single
+if [[ -z $2 ]]; then
+    QUERY="rpm -qai"
+else
+    QUERY="rpm -qi $2"
+fi
+
+# Test for hosts file
+if [[ -f "$1" ]]; then
+   while read HOST ; do
+        echo "" | ssh $HOST $QUERY | get_sw ;
+    done < $1
+else
+    ssh $1 $QUERY | get_sw
+fi
